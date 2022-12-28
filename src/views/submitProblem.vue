cb6b4068edb77c365206928d659bca61cecbda87
baar-vendel <fhinnoigsr@gmail.com>
2022-12-29 01:11:15 +0900

    submitするURLがvalidか判定

--- src/views/submitProblem.vue
+++ src/views/submitProblem.vue
@@ -2,18 +2,80 @@
 <script setup>
 import { ref } from "vue"
 import { supabase } from "../supabase.js"
+import axios from "axios"
 
 // tasks: 登録したタスク情報
 const tasks = ref([])
 // task: inputから追加予定のタスク
 const task = ref("")
+const input = ref("")
 
 const getTasks = async () => {
 	let { data, error, status } = await supabase.from('tasks').select('*')
 	console.log(error)
   	tasks.value = data
 }
-getTasks()
+// getTasks()
+
+// chatGPTで生成、有能すぎて...
+function parseUrl(url) {
+	const regex = /^https:\/\/atcoder\.jp\/contests\/(.+)\/tasks\/(.+)$/
+	const match = regex.exec(url)
+	if (match) {
+		const [, contest, task] = match
+		return [contest, task]
+  	} else {
+    	return [null, null]
+  	}
+}
+
+// 使用例
+console.log(parseUrl("https://atcoder.jp/contests/agc060/tasks/agc060_f")) // ['agc060', 'agc060_f']
+console.log(parseUrl("https://atcoder.jp/contests/abc060/tasks/abc060_f")) // ['abc060', 'abc060_f']
+console.log(parseUrl("https://atcoder.jp/contests/agc060/tasks/agc060_g")) // ['agc060', 'agc060_g']
+console.log(parseUrl("https://atcoder.jp/contests/abc060/tasks/abc060_g")) // ['abc060', 'abc060_g']
+console.log(parseUrl("https://atcoder.jp/contests/contest1/tasks/task1")) // ['contest1', 'task1']
+console.log(parseUrl("https://atcoder.jp/contests/contest2/tasks/task2")) // ['contest2', 'task2']
+console.log(parseUrl("https://atcoder.jp/contests/contest3/tasks/task3")) // ['contest3', 'task3']
+console.log(parseUrl("https://atcoder.jp/contests/agc060/tasks")) // null
+console.log(parseUrl("https://atcoder.jp/contests/abc060/tasks")) // null
+
+
+
+// AtCoder ProblemsのAPI叩いてデータベースに登録
+const addProblem = async () => {
+	const [contestId, problemId] = parseUrl(input.value)
+	console.log(contestId)
+	console.log(problemId)
+	if (contestId === null || problemId === null) {
+		alert("Error! Invalid URL. Please put a valid problem URL. (ex. https://atcoder.jp/contests/abc999/tasks/abc999_x )")
+		return
+	}
+
+	const url = "https://kenkoooo.com/atcoder/resources/problems.json"
+    const params = JSON.stringify({
+		// image: image.value,
+    })
+
+    await axios
+        .get(url)
+        .then((response) => {
+            // ここで'data:image/pngbase64, ...'から始まる文字列がログに流れれば送信成功
+            // dogImage.value = response.data.message
+			console.log(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+	
+	// const { data, error } = await supabase
+	// 	.from('tasks')
+	// 	.insert([{ task: task.value }])
+	// 	.select('*')
+	// console.log(error)
+	// tasks.value.push(data[0])
+	// task.value = ''
+}
 
 const addTask = async () => {
 	const { data, error } = await supabase
@@ -58,12 +120,14 @@ const updateTask = async (task) => {
             <button @click="deleteTask(task.id)">削除</button>
         </li>
     </ul>
-    <form @submit.prevent="addTask">
+    <!-- <form @submit.prevent="addTask"> -->
+    <form @submit.prevent="addProblem">
         <div>
-            <input v-model="task" />
+            <!-- <input v-model="task" /> -->
+            <input v-model="input" placeholder="https://atcoder.jp/contests/agc060/tasks/agc060_f" />
         </div>
         <div>
-            <button type="submit">他横行</button>
+            <button type="submit">投稿</button>
         </div>
     </form>
     <RouterLink v-bind:to="{ path: '/' }">一覧に戻る</RouterLink>
