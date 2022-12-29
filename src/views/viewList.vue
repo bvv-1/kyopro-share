d8b9d3ab01d80988baf3187de3a10732142fd72f
baar-vendel <fhinnoigsr@gmail.com>
2022-12-29 11:45:57 +0900

    見やすさのためコードの順番を入れ替えただけ

--- src/views/viewList.vue
+++ src/views/viewList.vue
@@ -1,3 +1,67 @@
+<!-- javascriptを記述する場所 -->
+<script setup>
+import { ref } from "vue"
+import { supabase } from "../supabase.js"
+
+// tasks: 登録したタスク情報
+const tasks = ref([])
+// task: inputから追加予定のタスク
+const task = ref("")
+
+const getTasks = async () => {
+	let { data, error, status } = await supabase.from('problems').select('*')
+	console.log(error)
+	tasks.value = data
+}
+getTasks()
+
+const addTask = async () => {
+	const { data, error } = await supabase
+		.from('tasks')
+		.insert([{ task: task.value }])
+		.select('*')
+	console.log(error)
+	tasks.value.push(data[0])
+	task.value = ''
+}
+
+const deleteTask = async (id) => {
+	const { data, error } = await supabase
+		.from('tasks')
+		.delete()
+		.eq('id', id)
+		.select('id')
+	console.log(error)
+	const index = tasks.value.findIndex((task) => task.id === data[0].id)
+	tasks.value.splice(index, 1)
+}
+
+const updateTask = async (task) => {
+	const { data, error } = await supabase
+		.from('tasks')
+		.update({ completed: task.completed })
+		.eq('id', task.id)
+		.select('*')
+	console.log(error)
+	const currentTask = tasks.value.find((task) => task.id === data[0].id)
+	currentTask.completed = data[0].completed
+}
+</script>
+
+<script>
+	export default {
+		data: () => ({
+			links: [
+				"About",
+				"List",
+				"Submit",
+				"Queue",
+			],
+		}),
+	}
+</script>
+
+<!-- マークアップでhtmlを記述する場所 -->
 <!-- vuetifyのワイヤーフレーム -->
 <template>
 	<v-app id="inspire">
@@ -54,76 +118,7 @@
 		</v-main>
 	</v-app>
 
-
-</template>
-  
-<script>
-	export default {
-		data: () => ({
-			links: [
-				"About",
-				"List",
-				"Submit",
-				"Queue",
-			],
-		}),
-	}
-</script>
-<!-- javascriptを記述する場所 -->
-<script setup>
-import { ref } from "vue"
-import { supabase } from "../supabase.js"
-
-// tasks: 登録したタスク情報
-const tasks = ref([])
-// task: inputから追加予定のタスク
-const task = ref("")
-
-const getTasks = async () => {
-	let { data, error, status } = await supabase.from('problems').select('*')
-	console.log(error)
-	tasks.value = data
-}
-getTasks()
-
-const addTask = async () => {
-	const { data, error } = await supabase
-		.from('tasks')
-		.insert([{ task: task.value }])
-		.select('*')
-	console.log(error)
-	tasks.value.push(data[0])
-	task.value = ''
-}
-
-const deleteTask = async (id) => {
-	const { data, error } = await supabase
-		.from('tasks')
-		.delete()
-		.eq('id', id)
-		.select('id')
-	console.log(error)
-	const index = tasks.value.findIndex((task) => task.id === data[0].id)
-	tasks.value.splice(index, 1)
-}
-
-const updateTask = async (task) => {
-	const { data, error } = await supabase
-		.from('tasks')
-		.update({ completed: task.completed })
-		.eq('id', task.id)
-		.select('*')
-	console.log(error)
-	const currentTask = tasks.value.find((task) => task.id === data[0].id)
-	currentTask.completed = data[0].completed
-}
-
-
-</script>
-
-<!-- マークアップでhtmlを記述する場所 -->
-<!-- <template>
-    <h1>良問</h1>
+	<h1>良問</h1>
     <ul>
         <li v-for="task in tasks" :key="task.id">
             <span><input type="checkbox" v-model="task.completed" @change="updateTask(task)" /></span>
@@ -132,4 +127,4 @@ const updateTask = async (task) => {
         </li>
     </ul>
 	<RouterLink v-bind:to="{ path: '/submit' }">良問を登録する</RouterLink>
-</template> -->
+</template>
