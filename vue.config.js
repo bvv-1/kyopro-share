61dcaee30ff272d353c4d5be732da2f033982476
bvv-1 <fhinnoigsr@gmail.com>
2023-03-05 11:55:21 +0900

    Add comments for viewList.vue

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    watch: true,
+  },
 
   pluginOptions: {
     vuetify: {
